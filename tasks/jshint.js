13b5b0742e457c5a7c525696c51c0ee889bee821
Max Klenk <max@klenk.biz>
2015-07-05 01:58:17 +0200

    add support for folder based jshint configuratations

--- tasks/jshint.js
+++ tasks/jshint.js
@@ -6,7 +6,7 @@ var jshint = require('gulp-jshint');
 module.exports = function(gulp, src) {
   gulp.task('jshint', function() {
     return gulp.src(src)
-      .pipe(jshint('.jshintrc'))
+      .pipe(jshint())
       .pipe(jshint.reporter('jshint-stylish'));
   });
 };
