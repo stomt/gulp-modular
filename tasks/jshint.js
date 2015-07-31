44c04721c95ee81044bcb87fc05dd03cb6dde915
Max Klenk <max@klenk.biz>
2015-08-01 00:48:38 +0200

    import jshint-stylish with required instead to allow nesting the dependency

--- tasks/jshint.js
+++ tasks/jshint.js
@@ -1,12 +1,12 @@
 'use strict';
 
 var jshint = require('gulp-jshint');
-
+var stylish = require('jshint-stylish');
 
 module.exports = function(gulp, src) {
   gulp.task('jshint', function() {
     return gulp.src(src)
       .pipe(jshint())
-      .pipe(jshint.reporter('jshint-stylish'));
+      .pipe(jshint.reporter(stylish));
   });
 };
