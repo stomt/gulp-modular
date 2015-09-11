9295d57a656a1f1333f366a29363fc0170bd90a4
Max Klenk <max@klenk.biz>
2015-09-11 18:11:06 +0200

    Abort gulp-deploy if no deploy branch is specified, closes #23

--- tasks/gitDeploy.js
+++ tasks/gitDeploy.js
@@ -1,12 +1,18 @@
 'use strict';
 
-var ghPages = require('gulp-gh-pages');
+var ghPages = require('gulp-gh-pages'),
+  gutil = require('gulp-util');
 
 module.exports = function(gulp, dist, deployBranch) {
   gulp.task('git-deploy', ['build'], function() {
-    return gulp.src(dist + '/**/*')
-      .pipe(ghPages({
-        branch: deployBranch
-      }));
+    if (deployBranch) {
+      return gulp.src(dist + '/**/*')
+        .pipe(ghPages({
+          branch: deployBranch
+        }));
+    } else {
+      gutil.log('No deploy branch defined', gutil.colors.magenta('Did not deploy!'));
+      return false;
+    }
   });
 };
